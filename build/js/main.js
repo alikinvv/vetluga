14cbfd8f49d139a336982b717f297c660a56c9ad
valikin <valikin@knoema.com>
2021-02-15 17:20:39 +0500

    update

--- build/js/main.js
+++ build/js/main.js
@@ -2,3 +2,25 @@ $('body').on('click', '.menu__dropdown span', (e) => {
     $('.menu__dropdown').not($(e.currentTarget).parent()).removeClass('active');
     $(e.currentTarget).parent().toggleClass('active');
 });
+
+let aist = new Swiper('.aist .swiper-container', {
+    effect: 'fade',
+    navigation: {
+        nextEl: '.aist .swiper-button-next',
+        prevEl: '.aist .swiper-button-prev',
+    },
+    pagination: {
+        el: '.aist .swiper-pagination',
+    },
+});
+
+let ilich = new Swiper('.ilich .swiper-container', {
+    effect: 'fade',
+    navigation: {
+        nextEl: '.ilich .swiper-button-next',
+        prevEl: '.ilich .swiper-button-prev',
+    },
+    pagination: {
+        el: '.ilich .swiper-pagination',
+    },
+});
